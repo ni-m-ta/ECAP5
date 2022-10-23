e971c30ab73a90984c6c24a92c3c3915bfc66eff
ni-m-ta <tatsuyanisato@gmail.com>
2022-10-22 22:25:55 -0700

    add privacypolicy

--- app/home/urls.py
+++ app/home/urls.py
@@ -6,4 +6,5 @@ app_name = 'home'
 
 urlpatterns = [
     path("", views.home.as_view(), name="home"),
+    path("privacypolicy/", views.privacypolicy.as_view(), name="privacypolicy"),
 ]
--- app/home/views.py
+++ app/home/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class home(TemplateView):
     template_name = "home/home.html"
+
+
+class privacypolicy(TemplateView):
+    template_name = "home/privacypolicy.html"
