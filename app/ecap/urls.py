aa4afa7144f266788b347ba817d1e5d040687699
ni-m-ta <tatsuyanisato@gmail.com>
2023-03-29 01:38:16 -0700

    add adx.txt

--- app/ecap/urls.py
+++ app/ecap/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
@@ -27,6 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
+    path('ads.txt', RedirectView.as_view(url=staticfiles_storage.url('ads.txt')))
 ]
 
 
